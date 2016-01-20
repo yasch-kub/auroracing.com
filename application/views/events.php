44178747f1b5fbd28ef7827b1f1a0f4562478d8e
sergy <serg.yaschuk@gmail.com>
2016-01-20 19:00:57 +0200

    add events

--- application/views/events.php
+++ application/views/events.php
@@ -1,34 +1,95 @@
-<div class="row">
-    <div class="col-md-3">
-
+<div class="col-lg-8">
+    <h1>Blog Post Title</h1>
+    <p class="lead">
+        by <a href="#">Start Bootstrap</a>
+    </p>
+    <hr>
+    <p>
+        <i class="fa fa-clock-o"></i>
+        Posted on August 24, 2013 at 9:00 PM
+    </p>
+    <hr>
+        <div class="ws_thumbs" style="overflow: hidden;">
+            <div style="top: 0px; left: -188px; transition: 0ms linear;">
+                <a href="#" title="Boats in the bay" class=""><img src="http://wowslider.com/sliders/demo-35/data1/tooltips/bay.jpg" alt="scripts image">gallery for 100% images</a>
+                <a href="#" title="Palms and ocean" class=""><img src="http://wowslider.com/sliders/demo-35/data1/tooltips/ocean.jpg" alt="responsive text">image with title changes</a>
+                <a href="#" title="Amazing sunset" class=""><img src="http://wowslider.com/sliders/demo-35/data1/tooltips/sun.jpg" alt="free template web image">code for content</a>
+                <a href="#" title="Sandy shore by the ocean" class=""><img src="http://wowslider.com/sliders/demo-35/data1/tooltips/shore.jpg" alt="galleria generator">background fade effect</a>
+                <a href="#" title="Pier and sea" class=""><img src="http://wowslider.com/sliders/demo-35/data1/tooltips/sea.jpg" alt="free template with with 6 pages">get positions</a>
+                <a href="#" title="Ocean spray" class=""><img src="http://wowslider.com/sliders/demo-35/data1/tooltips/spray.jpg" alt="auto image download free">simple fade with play pause button</a>
+                <a href="#" title="Coast at sunset" class="ws_selthumb"><img src="http://wowslider.com/sliders/demo-35/data1/tooltips/sunset.jpg" alt="free transitions">templates with picture</a>
+                <a href="#" title="Palms and blue sky" class=""><img src="http://wowslider.com/sliders/demo-35/data1/tooltips/palms.jpg" alt="automated free downloads">gallery software</a>
+            </div>
+        </div>
+    <hr>
+        <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, vero, obcaecati, aut, error quam sapiente nemo saepe quibusdam sit excepturi nam quia corporis eligendi eos magni recusandae laborum minus inventore?</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, doloribus, dolorem iusto blanditiis unde eius illum consequuntur neque dicta incidunt ullam ea hic porro optio ratione repellat perspiciatis. Enim, iure!</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, nostrum, aliquid, animi, ut quas placeat totam sunt tempora commodi nihil ullam alias modi dicta saepe minima ab quo voluptatem obcaecati?</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, dolor quis. Sunt, ut, explicabo, aliquam tenetur ratione tempore quidem voluptates cupiditate voluptas illo saepe quaerat numquam recusandae? Qui, necessitatibus, est!</p>
+    <hr>
+    <div class="well">
+        <h4>Leave a Comment:</h4>
+        <form role="form">
+            <div class="form-group">
+                <textarea class="form-control" rows="3"></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
     </div>
-    <div class="col-md-9">
-        <div class="row" id="eventPost">
-            <div id="slider">
-                <div class="col-md-1 arrow">
-                    <i class="fa fa-angle-left"></i>
-                </div>
-                <div class="col-md-10" id="slidePhoto">
-                    <div id="containerPhoto">
-                        {% for photo in post.photos %}
-                            <div>
-                                <a href="#" class="thumbnail">
-                                    <img num="{{ loop.index + 1 }}" src="/application/data/posts/{{ post.id }}/photos{{ photo }}">
-                                </a>
-                            </div>
-                        {% endfor %}
-                    </div>
-                </div>
-                <div class="col-md-1 arrow">
-                    <i class="fa fa-angle-right"></i>
+    <hr>
+    <div class="media">
+        <a class="pull-left" href="#">
+            <img class="media-object" src="http://placehold.it/64x64" alt="">
+        </a>
+        <div class="media-body">
+            <h4 class="media-heading">
+                Start Bootstrap
+                <small>August 25, 2014 at 9:30 PM</small>
+            </h4>
+            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+        </div>
+    </div>
+    <!-- Comment -->
+    <div class="media">
+        <a class="pull-left" href="#">
+            <img class="media-object" src="http://placehold.it/64x64" alt="">
+        </a>
+        <div class="media-body">
+            <h4 class="media-heading">
+                Start Bootstrap
+                <small>August 25, 2014 at 9:30 PM</small>
+            </h4>
+            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo.
+            Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate
+            fringilla. Donec lacinia congue felis in faucibus.
+            <div class="media">
+                <a class="pull-left" href="#">
+                    <img class="media-object" src="http://placehold.it/64x64" alt="">
+                </a>
+                <div class="media-body">
+                    <h4 class="media-heading">Nested Start Bootstrap
+                        <small>August 25, 2014 at 9:30 PM</small>
+                    </h4>
+                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo.
+                    Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi
+                    vulputate fringilla. Donec lacinia congue felis in faucibus.
                 </div>
             </div>
         </div>
-
-        <div class="row">
-            <form role="form">
-                <input>
-            </form>
+    </div>
+</div>
+<!--Search-->
+<div class="col-md-4">
+    <div class="well">
+        <h4>Blog Search</h4>
+        <div class="input-group">
+            <input type="text" class="form-control">
+            <span class="input-group-btn">
+                <button class="btn btn-default" type="button">
+                    <i class="fa fa-search"></i>
+                </button>
+            </span>
         </div>
     </div>
 </div>
